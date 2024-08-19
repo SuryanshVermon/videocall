60d405bf2fabd6996ed871485d2452608cc58b1e
suryansh vermon <suryanshvermon@suryansh-MacBook-Air.local>
2024-08-19 23:55:07 +0530

    Initial commit

---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,37 @@
+import React from"react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+
+const RoomPage =() => {
+    const{ roomId}=useParams();
+
+
+    const myMeeting = async(element)=>{
+        const appID =1201230889;
+        const serverSecret="294cb923e1bb1196e2eab5e3aef169c1";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID,serverSecret,roomId,Date.now().toString(),"Suryansh Vermon");
+
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name :'Copy Link',
+                    url : `https://vc-theta.vercel.app//room/${roomId}`,
+                }
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.OneONoneCall,
+            },
+            showScreenSharingButton: true,
+
+        });
+
+    };
+
+    return <div>
+        <div ref={myMeeting} />
+    </div>
+};
+export default RoomPage;
